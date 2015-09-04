24cf815cef836baef74d997c4edc38b1e4e2b360
MatFur92 <mat_fur_92_pub@yahoo.it>
2015-09-04 11:42:16 +0200

    fix start server

--- codeDeployFolder/startServer.sh
+++ codeDeployFolder/startServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo /home/ubuntu/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+sudo nohup /home/ubuntu/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0  1>/dev/null 2>&1 &
