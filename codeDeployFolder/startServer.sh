150633f6317bfa1069db9cc27f0a597aaf91e536
shekhar <shekhar.samal@gmail.com>
2022-05-28 17:15:19 +0530

    commit93

--- codeDeployFolder/startServer.sh
+++ codeDeployFolder/startServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nohup /home/ubuntu/wildfly-9.0.1.Final/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0  1>/dev/null 2>&1 &
+sudo nohup /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0  1>/dev/null 2>&1 &
